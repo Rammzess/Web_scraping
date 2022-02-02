41d35b06760ff999af1c096b0dcb687619fa3851
Dmitry Dunin <dmitrry.dunin@gmail.com>
2022-02-02 19:52:18 +0200

    Initial commit

---
+++ Headers.py
@@ -0,0 +1,18 @@
+HEADERS = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Cookie': '_ga=GA1.2.1983396850.1642165642; hl=ru; fl=ru; _ym_uid=1642165642251792474; _ym_d=1642165642; __gads=ID=60ce575f18d4aafc:T=1642165642:S=ALNI_MZ3zALn6OdBZ5uAZNyOp8rqrhrySw; visited_articles=132554:349860; habr_web_home=ARTICLES_LIST_ALL; _gid=GA1.2.1352512644.1643653067; cto_bundle=UBvwwF9LREU0RHZQNFhHaTdVbjRRRDZUaHNRT0YlMkIyb2wlMkZlUUR0SGlSUGJab1E5d1d4MHBmR2o1RXJmRHBzTEdXJTJCcjRXellGdFRiRVFJQzNjM2V4cnFaRXBiYm9raHNRWUZIcWNnWEdZTk5oYTl6OUNJJTJCMlVXdW0lMkJsQUoyOUkzbWFUS0pmQzk4ZnlPa1ZkNVRLekglMkJVa2hBUEElM0QlM0Q; _ym_isad=2',
+    'Host': 'habr.com',
+    'sec-ch-ua': '"Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': "Windows",
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
+}
---
+++ Web-scraping.py
@@ -0,0 +1,31 @@
+KEYWORDS = ['дизайн', 'фото', 'web', 'python']
+
+from bs4 import BeautifulSoup
+import requests
+import pprint
+from Headers import HEADERS
+
+
+
+page = requests.get('https://habr.com/ru/rss/all/all/?fl=ru', headers=HEADERS)
+soup = BeautifulSoup(page.text, 'html.parser')
+
+
+posts = soup.find_all('item')
+for post in posts:
+    post_url = post.guid.string
+    post_date = post.pubdate.string
+    if not post_url:
+        continue
+    post_id = int(post_url.split('/')[-2])
+    post_date = post_date.split(', ')[-1]
+
+#    извлекаем хабы поста
+    hubs = post.find_all('category')
+    for hub in hubs:
+       corrected_hub = hub.string.lower()
+       if any([corrected_hub in chosen for chosen in KEYWORDS]):
+           title_element = post.find('title')
+           print(f"\nDATE : {post_date}, \nTitle: {title_element.text}, \
+               \nLINK: {post.guid.string}")
+           break
